a82721f9d6feae7ea4571e0709c57f7f89615ab2
Pandakik <71925880+Pandakik@users.noreply.github.com>
2021-02-21 22:35:45 +0800

    Add files via upload

---
+++ a_plus_b_problem.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+void sayhello();
+int main()
+{    
+    int a,b;//定义常量 
+    while (cin >> a >> b)
+    {
+        cout << a + b << endl; 
+    }
+    return 0;
+}
